80d0dfc0bc8a0dccaca6f53da218a22cf9ce8886
Maxim Uvarov <maxim-uvarov@users.noreply.github.com>
2023-05-14 10:19:43 +0000

    v0.3.2.1

--- cyber.rb
+++ cyber.rb
@@ -4,8 +4,8 @@
 class Cyber < Formula
   desc ""
   homepage ""
-  url "https://github.com/cyber-prophet/go-cyber-for-brew/archive/refs/tags/v0.3.2dragonberry.tar.gz"
-  sha256 "9f0a9c3fcff6dd768963bde6c2ac28275f39c25b2a66fe6ff61a243af86f26e5"
+  url "https://github.com/cyber-prophet/go-cyber-for-brew/archive/refs/tags/v0.3.2.1.tar.gz"
+  sha256 "0ba0f9737185c6aaf659a745d2eac2ed358588b7d5987b7fb6b1129e489e78af"
   license ""
 
   depends_on "go" => :build
@@ -19,9 +19,9 @@ class Cyber < Formula
     bin_path = buildpath/"src/github.com/cyber-prophet/cyber"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "go", "build", "-o", bin/"cyber", "./cmd/cyber"
+      # system "go", "install", "-o", bin/"cyber", "./cmd/cyber"
+      system "make", "install-cli" 
     end
-    # system "make", "install", "SOFTWARE_2_ENABLED=false"
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
