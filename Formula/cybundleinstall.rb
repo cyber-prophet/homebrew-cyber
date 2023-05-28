bbb09e51dbae283fbbd08e002e2af5dfa279f4b5
Maxim Uvarov <maxim-uvarov@users.noreply.github.com>
2023-05-28 14:26:30 +0000

    cybundleinstall 013

--- Formula/cybundleinstall.rb
+++ Formula/cybundleinstall.rb
@@ -4,51 +4,24 @@
 class Cybundleinstall < Formula
   desc "Cy - the hacker's browser of Cyber 🔵 superintelligence blockchains. Nushell based."
   homepage "https://github.com/cyber-prophet/cy/"
-  url "https://raw.githubusercontent.com/cyber-prophet/homebrew-taps/main/releases/cybundleinstall-012.tar.gz"
-  sha256 "0c0cd9979778afcd7f7e947fa80c7e4bc022de6089a39f9dcf174984ac19dfa8"
+  url "https://raw.githubusercontent.com/cyber-prophet/homebrew-taps/main/releases/cybundleinstall-013.tar.gz"
+  sha256 "e1bcf9aaec26d33657281d0a36843848443d43a7a3924d4ee8ab473b3d52f39a"
   license "MIT"
 
-  # depends_on "nushell" 
-  # depends_on "cyber" 
-  # depends_on "pussy" 
-  # depends_on "curl" 
-  # depends_on "pueue" 
-  # depends_on "ipfs"
+  depends_on "nushell" 
+  depends_on "cyber" 
+  depends_on "pussy" 
+  depends_on "curl" 
+  depends_on "pueue" 
+  depends_on "ipfs"
   # depends_on "gum"
   
 
-  # on_macos do 
-  #   depends_on ""
-  # end
-  # depends_on cask: "wezterm" 
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    # bin_path = buildpath/"src/github.com/cyber-prophet/cy"
-    # bin_path.install Dir["*"]
-    
     bin.install 'cybundleinstall'
-    # system "nu", "-c", "'cp brew_scripts/cy.sh #{bin}/cy'"
-
-    # cd bin_path do
-    #   # system "nu", "-c", "'cp brew_scripts/cy.sh #{bin}/cy'"
-    # end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cy`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
