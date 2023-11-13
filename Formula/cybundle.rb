1ae7c232331121593a3f7bdb1b6eed513d551d8d
Maxim Uvarov <maxim-uvarov@users.noreply.github.com>
2023-11-13 06:01:32 +0000

    cybundle 046

--- Formula/cybundle.rb
+++ Formula/cybundle.rb
@@ -3,8 +3,8 @@
 class Cybundle < Formula
   desc "cy - access Cybergraph🔵 from Nushell. Query and manage the Bostrom Consensus computer. Your decentralized superintelligence."
   homepage "https://github.com/cyber-prophet/cy/"
-  url "https://raw.githubusercontent.com/cyber-prophet/homebrew-taps/main/releases/cybundle-045.tar.gz"
-  sha256 "ea6181832c89bd11064e50785bfd6a38a5e845b099588e86fd7a2c307bdba97b"
+  url "https://raw.githubusercontent.com/cyber-prophet/homebrew-taps/main/releases/cybundle-046.tar.gz"
+  sha256 "65d424f2092d930c8dedd9c7037c31c9e3ccfcb0a4bacd61f6bf862c0ef41fd7"
   license "MIT"
 
   depends_on "nushell"
